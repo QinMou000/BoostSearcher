9829147b30f00f504c45c892b4349d85073a2d4e
wangqin <2830862261@qq.com>
2025-09-28 19:42:21 +0800

    fix 搜索出同一文档的问题

--- searcher.hpp
+++ searcher.hpp
@@ -17,6 +17,11 @@ namespace ns_searcher
         {
             // 获取index单例
             index = ns_index::Index::GetInstance();
+            if (index == nullptr)
+            {
+                std::cerr << "获取单例失败" << std::endl;
+                exit(1);
+            }
             std::cout << "获取单例成功" << std::endl;
             // 建立索引
             index->BuildIndex(raw_file_path);
@@ -29,8 +34,17 @@ namespace ns_searcher
             std::vector<std::string> words;
             Jieba_util::CutString(query, &words);
 
+            struct invertedElem_to_merge
+            {
+                uint64_t doc_id;                // 文档id
+                int sum_weight;                 // 这个文档在本次搜索中占的总权值
+                std::vector<std::string> Words; // 本次搜索被切分的词中和文档内容相关的词
+            };
+
             // 根据切词结果 到索引里面去找
-            ns_index::InvertedList inverted_list_all;
+            // ns_index::InvertedList inverted_list_all;
+
+            std::unordered_map<uint64_t, invertedElem_to_merge> map; // 避免多个词搜索到同一个文档的情况
 
             for (std::string word : words)
             {
@@ -38,15 +52,29 @@ namespace ns_searcher
                 ns_index::InvertedList *inverted_list = index->GetInvertedList(word); // 拿到倒排拉链
                 if (inverted_list == nullptr)
                     continue;
-                inverted_list_all.insert(inverted_list_all.end(), inverted_list->begin(), inverted_list->end()); // 将找到的倒排拉链里面的元素都插进新的拉链里面
+                for (auto &item : *inverted_list)
+                {
+                    // 将拿到的倒排拉链中的元素插入到hash里面去重
+                    auto &Second = map[item.doc_id]; // 需要加引用 不加引用并不会改变map里面的值
+                    Second.doc_id = item.doc_id;
+                    Second.sum_weight += item.weight;  // 将所有文档的权值累加
+                    Second.Words.emplace_back(item.word); // 依旧减少拷贝
+                }
+
+                // inverted_list_all.insert(inverted_list_all.end(), inverted_list->begin(), inverted_list->end()); // 将找到的倒排拉链里面的元素都插进新的拉链里面
+            }
+            std::vector<invertedElem_to_merge> inverted_list_all;
+            for (auto &item : map)
+            {
+                inverted_list_all.emplace_back(item.second);
             }
             // 保留100个结果
-            inverted_list_all.resize(50);
+            inverted_list_all.resize(100);
 
             // 根据找到的结果 按照权重降序排序
             std::sort(inverted_list_all.begin(), inverted_list_all.end(),
-                      [](const ns_index::InvertedElem &e1, const ns_index::InvertedElem &e2)
-                      { return e1.weight > e2.weight; });
+                      [](const invertedElem_to_merge &e1, const invertedElem_to_merge &e2)
+                      { return e1.sum_weight > e2.sum_weight; });
             // 构建 json 串并返回
             Json::Value root;
             for (auto &item : inverted_list_all) // 已经是按降序排列好的
@@ -56,7 +84,11 @@ namespace ns_searcher
                     continue;
                 Json::Value it; // 为单个文档构建 json
                 it["title"] = doc->title;
-                it["desc"] = GetAbstract(doc->content /*这里不能拿网页原始内容需要转小写 */, item.word); // TODO 不能把全部内容都加进去 // 需要获取摘要
+                if (item.Words.empty())
+                    std::cerr << "No relavent words" << std::endl;
+                else
+                    it["desc"] = GetAbstract(doc->content /*这里不能拿网页原始内容需要转小写 */,
+                                             item.Words[0] /*数组第一个词传进去找*/); // TODO 不能把全部内容都加进去 // 需要获取摘要
                 it["url"] = doc->url;
 
                 // FOR DEBUG
