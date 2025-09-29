a38bdbe6ee8a018bf47080445b62927befebc3e0
wangqin <2830862261@qq.com>
2025-09-29 20:47:39 +0800

    update 添加日志

--- log.hpp
+++ log.hpp
@@ -3,3 +3,15 @@
 #include <iostream>
 #include <string>
 #include <time.h>
+
+#define NORMAL  1
+#define WARNING 2
+#define DEBUG   3
+#define FATAL   4
+
+#define LOG(LEVEL, MESSAGE) log(#LEVEL, MESSAGE, __FILE__, __LINE__)
+
+void log(std::string level,std::string mesg,std::string file,std::string line)
+{
+    std::cout << "[" << level << "]" << "[" << time(nullptr) << "]" << "[" << mesg << "]" << "[" << file << " : " << line << "]" << std::endl;
+}
